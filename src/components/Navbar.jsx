bb66bffef89759b3d1be7012964810964fbe370d
saeid1994 <saeedsnaid@gmail.com>
2024-06-30 01:27:52 +0330

    adding new chages in nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,18 @@
+import Image from "next/image";
+
 export default function Navbar(props) {
-  return <p className="">navbar</p>;
+  return (
+    <nav className="w-full h-full bg-green-400 flex px-[14rem]">
+      <div className="w-full h-full bg-red-400 basis-[80%]">
+        {/* container of image */}
+        <div className="w-full h-full bg-red-950 relative">
+          <Image 
+          width={100}
+          height={100}
+           src="/assets/Screenshot 2024-06-30 at 12.46.18 AM.png" />
+        </div>
+      </div>
+      <div className="w-full h-full bg-red-700 basis-[20%]">22</div>
+    </nav>
+  );
 }
