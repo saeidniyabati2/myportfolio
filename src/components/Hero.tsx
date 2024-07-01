f940a35bf75cd25e7f84e22d033453487ba577d8
saeid1994 <saeedsnaid@gmail.com>
2024-07-02 00:05:58 +0330

    installing mui, adding some icon svg

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,5 @@
+import Button from "@mui/material/Button";
+import Image from "next/image";
 export default function Hero(props: object) {
   // let test = "this is test";
 
@@ -34,16 +36,34 @@ export default function Hero(props: object) {
   return (
     <section className="w-full h-[75vh] bg-red-600 flex">
       {/* left side section */}
-      <div className="w-full h-full bg-red-400 basis-[73%] bg-[#F8F9FD]">
+      <div className="w-full h-full basis-[73%] bg-[#F8F9FD]">
         dfd
       </div>
       {/* right side section */}
       <div className="w-full h-full  basis-[27%] bg-[#ECF6FF] relative">
-        <div className="w-9 h-9 bg-white absolute top-20 right-20 shadow1">
-          df
+        <div className="w-12 h-12 bg-white absolute top-20 right-20 shadow1  flex items-center justify-center p-1 rounded-sm">
+          <Image
+            width={100}
+            height={100}
+            alt="ddf"
+            src="/assets/React-icon.svg.png"
+          />
         </div>
-        <div className="w-6 h-6 bg-white absolute top-32 right-40 shadow1">
-          df
+        <div className="w-6 h-6 bg-white absolute top-32 right-56 shadow1 p-1 rounded-sm ">
+          <Image
+            width={100}
+            height={100}
+            alt="ddf"
+            src="/assets/tailwindcss-icon-svgrepo-com.svg"
+          />
+        </div>
+        <div className="w-10 h-10 bg-white absolute top-56 right-32 shadow1  flex items-center justify-center p-1 rounded-sm">
+          <Image
+            width={100}
+            height={100}
+            alt="ddf"
+            src="/assets/next-dot-js-svgrepo-com.svg"
+          />
         </div>
       </div>
     </section>
