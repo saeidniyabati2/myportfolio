683459604923859d0aad0d36ae97f19786eecffc
saeid1994 <saeedsnaid@gmail.com>
2024-07-02 19:17:50 +0330

    adding button

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,5 @@
 import Button from "@mui/material/Button";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import Image from "next/image";
 export default function Hero(props: object) {
   // let test = "this is test";
@@ -34,10 +35,35 @@ export default function Hero(props: object) {
   // type test11 = 1 | 3;
 
   return (
-    <section className="w-full h-[75vh] bg-red-600 flex">
+    <section className="w-full h-[75vh] bg-green-600 flex ">
       {/* left side section */}
-      <div className="w-full h-full basis-[73%] bg-[#F8F9FD] relative">
-        <div className="w-10 h-10 bg-white absolute top-24 right-52 shadow1  flex items-center justify-center p-1 rounded-sm">
+      <div className="w-full h-full basis-[73%] bg-[#F8F9FD] relative pl-[15rem]">
+        {/* container of texts with buttons */}
+        <div className="w-full  bg-red-500 flex flex-col gap-y-5">
+          <h1 className="mt-32 text-5xl">Hello Mate 👋</h1>
+          <h1 className="text-6xl">I'm Saeid Niyabati</h1>
+          <h1 className="text-6xl">
+            a <span className="text-[#2383C4]">Web Developer</span>
+          </h1>
+          <p className="text-[#4D5661] max-w-[30rem] text-sm leading-relaxed">
+            Hi, I'm Saeed a freelancer web developer from Iran. I help brands
+            turn their idea into high quality products.
+          </p>
+          {/* buttons container */}
+          <div className="w-full bg-green-700">
+            <Button
+              variant="contained"
+              endIcon={
+                <ArrowForwardIosIcon  fontSize="small" className="text-sm" />
+              }
+              size="large"
+              className="px-8 text-xs"
+            >
+              Send
+            </Button>
+          </div>
+        </div>
+        <div className="w-10 h-10 bg-white absolute top-24 right-32 shadow1  flex items-center justify-center p-1 rounded-sm">
           <Image
             width={100}
             height={100}
@@ -45,8 +71,7 @@ export default function Hero(props: object) {
             src="/assets/node-js-svgrepo-com.svg"
           />
         </div>
-
-        <div className="w-12 h-12 bg-white absolute top-52 right-72 shadow1  flex items-center justify-center p-1 rounded-sm">
+        <div className="w-12 h-12 bg-white absolute top-52 right-52 shadow1  flex items-center justify-center p-1 rounded-sm">
           <Image
             width={100}
             height={100}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import ContainerBlock from "../components/ContainerBlock";
 import Hero from "../components/Hero";
 
-const Home = (props) => {
+const Home = (props:object) => {
   return (
     <ContainerBlock>
       <Hero />
