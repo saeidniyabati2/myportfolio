ac7bc8bb23e3ac075219b5e6dcfdec368b4141d9
saeid1994 <saeedsnaid@gmail.com>
2024-07-11 23:50:32 +0330

    adding comment

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import AboutMe from "../components/AboutMe";
 import ContainerBlock from "../components/ContainerBlock";
 import Hero from "../components/Hero";
 import SkillSet from "../components/SkillSet";
+// commetn
 
 const Home = (props: object) => {
   return (
