e0884c317109cd7fe9356e5fe75628f8cf61d8a7
saeid1994 <saeedsnaid@gmail.com>
2024-07-13 20:28:30 +0330

    adding new component for experties cards

--- src/components/Experties.tsx
+++ src/components/Experties.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { ExpertiesCard } from "./cards/ExpertiesCard";
 
 export const Experties: React.FC = () => {
   return (
@@ -13,68 +14,29 @@ export const Experties: React.FC = () => {
       {/* container of of cards */}
       <div className="w-full flex pt-10 gap-x-4">
         {/* first card */}
-        <div className="w-full  flex border-gray-200 border p-6 py-7 gap-x-4 rounded-[0.200rem]">
-          {/* container of icon */}
-          <div className="w-11 h-10 rounded-full bg-blue-100 p-[0.55rem] flex items-center justify-center">
-            <Image
-              width={50}
-              height={50}
-              alt="icon"
-              src="/assets/command-svgrepo-com.svg"
-            />
-          </div>
-          {/* container texts */}
-          <div className="w-full flex flex-col gap-y-3">
-            <h5>Web Design</h5>
-            <p className="text-secondGray  text-[0.8rem] leading-relaxed ">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem,
-              elit. Quidem,
-            </p>
-            <p className="  text-sm">Explore Service</p>
-          </div>
-        </div>
+        <ExpertiesCard
+          title="Web Design"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem,
+              elit. Quidem,"
+          miniText="Explore Service"
+          status={false}
+        />
         {/* second card */}
-        <div className="w-full  flex border-gray-200 border p-6 py-7 gap-x-4 rounded-[0.200rem]">
-          {/* container of icon */}
-          <div className="w-11 h-10 rounded-full bg-blue-100 p-[0.55rem] flex items-center justify-center">
-            <Image
-              width={50}
-              height={50}
-              alt="icon"
-              src="/assets/command-svgrepo-com.svg"
-            />
-          </div>
-          {/* container texts */}
-          <div className="w-full flex flex-col gap-y-3">
-            <h5>Web Design</h5>
-            <p className="text-secondGray   text-[0.8rem] leading-relaxed ">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem,
-              elit. Quidem,
-            </p>
-            <p className="  text-sm">Explore Service</p>
-          </div>
-        </div>
+        <ExpertiesCard
+          title="Web Design"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem,
+              elit. Quidem,"
+          miniText="Explore Service"
+          status={true}
+        />
         {/* third card */}
-        <div className="w-full  flex border-gray-200 border p-6 py-7 gap-x-4 rounded-[0.200rem]">
-          {/* container of icon */}
-          <div className="w-11 h-10 rounded-full bg-blue-100 p-[0.55rem] flex items-center justify-center">
-            <Image
-              width={50}
-              height={50}
-              alt="icon"
-              src="/assets/command-svgrepo-com.svg"
-            />
-          </div>
-          {/* container texts */}
-          <div className="w-full flex flex-col gap-y-3">
-            <h5>Web Design</h5>
-            <p className="text-secondGray  text-[0.8rem] leading-relaxed ">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem,
-              elit. Quidem,
-            </p>
-            <p className="  text-sm">Explore Service</p>
-          </div>
-        </div>
+        <ExpertiesCard
+          title="Web Design"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem,
+              elit. Quidem,"
+          miniText="Explore Service"
+          status={false}
+        />
       </div>
     </section>
   );
---
+++ src/components/cards/ExpertiesCard.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+
+interface props {
+  title: string;
+  description: string;
+  miniText: string;
+  status: boolean;
+}
+
+export const ExpertiesCard: React.FC<props> = ({
+  title,
+  description,
+  miniText,
+  status,
+}) => {
+  return (
+    <div
+      className={`w-full  flex border-gray-200 border p-6 py-7 gap-x-4 rounded-[0.200rem] ${
+        status && "border-0 expertiesCardShadow"
+      }`}
+    >
+      {/* container of icon */}
+      <div
+        className={`w-11 h-10 rounded-full bg-blue-100 p-[0.55rem] flex items-center justify-center ${
+          status && "bg-mainButton"
+        }`}
+      >
+        <Image
+          width={50}
+          height={50}
+          alt="icon"
+          src="/assets/command-svgrepo-com.svg"
+        />
+      </div>
+      {/* container texts */}
+      <div className="w-full flex flex-col gap-y-3">
+        <h5>{title}</h5>
+        <p className="text-secondGray  text-[0.8rem] leading-relaxed ">
+          {description}
+        </p>
+        <div className="flex flex-col ">
+          <p
+            className={`text-sm ${
+              status &&
+              "text-mainButton"
+            }`}
+          >
+            {miniText}
+          </p>
+          <hr className={`w-[6.3rem] h-[0.08rem]  ${status ? "visible bg-mainButton  border-none" :  "invisible"}`}/>
+        </div>
+      </div>
+    </div>
+  );
+};
