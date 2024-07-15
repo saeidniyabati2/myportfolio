bd14608ba47d239379608384e6628330a3725421
saeid1994 <saeedsnaid@gmail.com>
2024-07-16 01:31:43 +0330

    remove carouse image react

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,7 +1,12 @@
+
+import { useState } from "react";
+
 export const Comments: React.FC = () => {
+
+
   return (
-    <section className="w-full px-[15rem] bg-red-700">
-        dfd
+    <section className="w-full px-[15rem] bg-red-300">
+     
     </section>
   );
 };
