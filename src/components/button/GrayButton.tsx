cb942264b913972d0d47b4576339d89a43e12d90
saeid1994 <saeedsnaid@gmail.com>
2024-07-24 22:07:11 +0330

    ..

--- src/components/button/GrayButton.tsx
+++ src/components/button/GrayButton.tsx
@@ -9,7 +9,7 @@ export const GrayButton: React.FC<props> = ({ title, Icon }) => {
   return (
     <Button
       variant="outlined"
-      className="  bg-[#F8F9FD]  px-6 py-2 rounded-sm border-[#ADB7C4] outline-none hover:bg-[#ffffff] hover:border-[#ADB7C4]"
+      className="bg-[#F8F9FD]  px-6 py-2 rounded-sm border-[#ADB7C4] outline-none hover:bg-[#ffffff] hover:border-[#ADB7C4]"
       endIcon={<Icon style={{ fontSize: "14px", color: "#29303B" }} />}
     >
       <p className="font-HankenGrotesk no-transform font-light text-[#29303B]">
